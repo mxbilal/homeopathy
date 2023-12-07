12a006f03687f9c7899804e5e0e1729c430b90a9
bilal <mxbilal0@gmail.com>
2023-12-07 17:38:41 +0500

    push

--- src/components/FooterPage.tsx
+++ src/components/FooterPage.tsx
@@ -1,3 +1,6 @@
+import lnIcon from "../assets/linkedin.png";
+import instaIcon from "../assets/instagram.png";
+import twIcon from "../assets/twitter.png";
 const FooterPage = () => {
   return (
     <footer className="bg-gray-800 text-white p-8 mb-auto">
@@ -30,16 +33,30 @@ const FooterPage = () => {
           <h2 className="text-2xl font-semibold mb-4">Developer Info</h2>
           <p>Email: mxbilal0@gmail.com</p>
           <p>Phone: +923495683110</p>
-          <p>
-            LinkedIn:{" "}
+          <div className="flex gap-4 items-center mt-4">
             <a
               href="https://www.linkedin.com/in/mxbilal0/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              mxbilal0
+              <img src={lnIcon} width={20} />
             </a>
-          </p>
+
+            <a
+              href="https://twitter.com/mxbilal0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twIcon} width={20} />
+            </a>
+            <a
+              href="https://www.instagram.com/mbilal22232/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instaIcon} width={20} />
+            </a>
+          </div>
         </div>
       </div>
       <div className="border-t border-gray-600 mt-8 pt-4 text-center">
